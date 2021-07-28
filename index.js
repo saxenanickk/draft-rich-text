327be708988e86a349b2b953557dd6c4c20b9d3a
saxenanickk <saxenanickk@gmail.com>
2021-07-28 15:11:35 +0530

    export chang4ed

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import TextEditor from "./src/TextEditor";
-import RichText from "./src/RichTextRenderer";
-export { TextEditor, RichText };
+export {TextEditor} from "./src/TextEditor";
+export {RichText} from "./src/RichTextRenderer";
